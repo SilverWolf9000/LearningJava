74515d9181700566f087b621afddac8cffeab7a0
Jacob Riecke <jwriecke98@gmail.com>
2014-10-23 22:29:08 -0400

    Hello World
    First Java program displaying the use of comments and print line code

---
+++ Application.java
@@ -0,0 +1,11 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// This is the basic print/writeline of Java programming. Simple, really.
+		System.out.println("Hello World!");
+		System.out.println("This is my very first Java program.");
+		System.out.println("I am currently running Eclipse Luna with Java 8.");
+	}
+
+}
