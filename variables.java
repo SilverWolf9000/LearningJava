df5168112fd8ecfd3ff62a7205082079ce904118
Jacob Riecke <jwriecke98@gmail.com>
2014-10-25 16:44:20 -0400

    Variables
    Displays and defines all 8 types of variables in Java

---
+++ variables.java
@@ -0,0 +1,42 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// Variables are like containers; there are 8 types of var
+		
+		// int - whole number; 32-bit value
+		int myNumber = 88;
+		
+		// short - 16-bit value
+		short myShort = 847;
+		
+		// long - 64-bit value
+		long myLong = 9797;
+		
+		// double - used for floating point numbers (double precision)
+		double myDouble = 7.3243;
+		
+		// float - shorter double
+		float myFloat = 324.3f;
+		
+		// char - unicode characters; 16-bit value
+		char myChar = 'y';
+		
+		// boolean - true or false
+		boolean myBoolean = true;
+		
+		// byte - 8-bit value
+		byte myByte = 127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
