164c469fbb40e454655daa552a8c01f48cd89eaf
Jacob Riecke <jwriecke98@gmail.com>
2014-10-26 15:18:32 -0400

    User Input
    Shows how to allow user to put in text/number values

---
+++ userinput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class userinput {
+	public static void main(String[] args) {
+		// getting user input - using system input (system.in)
+		
+		// Create scanner object (resource leak never fixed in video...)
+		Scanner input = new Scanner(System.in);
+		
+		// Output the prompt
+		System.out.println("Enter a floating point value: ");
+		
+		// Wait for the user to enter something
+		double value = input.nextDouble();
+		
+		// Tell them what they entered
+		System.out.println("You entered: " + value);
+	}
+}
