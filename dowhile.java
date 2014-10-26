1408a5711a4faa93ff09401df3c6b898ac90543a
Jacob Riecke <jwriecke98@gmail.com>
2014-10-26 15:31:50 -0400

    Do While
    Code executed until a certain condition is met

---
+++ dowhile.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class dowhile {
+	public static void main(String[] args) {
+		// do while - loops code until condition met
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		/*
+		System.out.println("Enter a number: ");
+		int value = scanner.nextInt();
+		
+		while(value != 5) {
+			System.out.println("Enter a number: ");
+			value = scanner.nextInt();
+		}
+		*/
+		
+		int value = 0;
+		
+		do {
+			System.out.println("Enter a number: ");
+			value = scanner.nextInt();
+		}
+		
+		while(value != 5);
+		
+		System.out.println("Got 5!");
+	}
+}
