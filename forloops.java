0050e0580bc03a6730cea3e98bf3c4d84bbde7e7
Jacob Riecke <jwriecke98@gmail.com>
2014-10-25 22:20:36 -0400

    For Loops
    Display of the practical use of for loops in beginning

---
+++ forloops.java
@@ -0,0 +1,11 @@
+public class forloops {
+
+	public static void main(String[] args) {
+		// forloops - sec. 1 declare var; sec. 2 give condition for validity; sec. 3 change for each postexecution
+		for(int i = 0; i < 5; i++) {
+			// printf specifies specific value using %d for int and \n = new line
+			System.out.printf("The value of i is %d\n", i);
+		}
+	}
+
+}
